0f30be06756a6b471e33be560d5c0cca3693db68
lukasz-szewc <lukasz-szewc@users.noreply.github.com>
2016-04-17 11:23:46 +0200

    added spring boot into project

---
+++ src/main/java/org/luksze/SimpleSpringApp.java
@@ -0,0 +1,12 @@
+package org.luksze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringApp {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SimpleSpringApp.class, args);
+    }
+}
