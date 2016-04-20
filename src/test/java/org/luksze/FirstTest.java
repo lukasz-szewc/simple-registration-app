63bb53c345756c69338b3ca0c8e4ffa6ba82a9ac
lukasz-szewc <lukasz-szewc@users.noreply.github.com>
2016-04-20 11:07:12 +0200

    added junit in order to check how gradle works with both spock and junit

---
+++ src/test/java/org/luksze/FirstTest.java
@@ -0,0 +1,11 @@
+package org.luksze;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FirstTest {
+    @Test
+    public void testName() throws Exception {
+        Assert.assertTrue(true);
+    }
+}
